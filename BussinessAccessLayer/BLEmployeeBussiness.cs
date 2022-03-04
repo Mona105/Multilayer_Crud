6c66b10caadfc051c2f1c4d2c98e14f62b7c93b8
ROY <ROY@DESKTOP-26H23PD>
2022-03-04 12:35:51 +0530

    Add project files.

---
+++ BussinessAccessLayer/BLEmployeeBussiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommanLayer.Models;
+using DataLayer;
+using System.Collections.Generic;
+
+namespace BussinessAccessLayer
+{
+    public class BLEmployeeBussiness
+    {
+        private EmployeeDataAccess employeeData;
+        public BLEmployeeBussiness()
+        {
+            employeeData = new EmployeeDataAccess();
+        }
+        public List<Employees> GetEmployees()
+        {
+            return employeeData.GetEmployees();
+        }
+    }
+}
---
+++ DataLayer/EmployeeDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommanLayer.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+namespace DataLayer
+{
+    public class EmployeeDataAccess
+    {
+        private DbConnection db = new DbConnection();
+        public List<Employees> GetEmployees()
+        {
+            string query = "select * from Employee";
+            SqlCommand command = new SqlCommand();
+            command.CommandText = query;
+            command.Connection = db.cnn;
+            if(db.cnn.State==System.Data.ConnectionState.Closed)
+            {
+                db.cnn.Open();
+            }
+            SqlDataReader reader = command.ExecuteReader();
+            List<Employees> employees = new List<Employees>();
+            while(reader.Read())
+            {
+                Employees emp = new Employees();
+                emp.Id = (int)reader["Id"];
+                emp.Name = reader["Name"].ToString();
+                emp.Email = reader["Email"].ToString();
+                emp.Gender = reader["Gender"].ToString();
+                emp.Mobile = reader["Mobile"].ToString();
+            }
+            db.cnn.Close();
+            return employees;
+        }
+    }
+}
